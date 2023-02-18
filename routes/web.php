1e5474345d967b463f241b1d52f418dcbdc0382f
Maishmaina <simpledaniel.1818@gmail.com>
2023-02-18 10:50:44 +0300

    routing and route update

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Auth::routes();
-
-// Route::get('/home', 'HomeController@index')->name('home');
+// prevent not found error
+Route::get('/{vue_capture?}',function () {
+        return view('welcome');
+    })->where('vue_capture', '[\/\w\.-]*');
